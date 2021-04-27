f2acf496e1f09f8c480c5899a0ae998bf15a25e6
7karimia <jurik.karimian@uni-hamburg.de>
2021-04-27 09:54:51 +0200

    Fixed winding order of box with one normal per unique vertex

--- web/common/BoxObject.js
+++ web/common/BoxObject.js
@@ -225,18 +225,18 @@ export default class Box extends GameObject {
         // // Triangles with correct winding order
         // // (counter clockwise is front facing)
         // const indices = [
-        //     0, 1, 3,
-        //     1, 2, 3,
-        //     0, 3, 4,
-        //     3, 7, 4,
-        //     4, 7, 5,
-        //     7, 6, 5,
-        //     0, 4, 1,
-        //     1, 4, 5,
-        //     1, 5, 2,
-        //     5, 6, 2,
-        //     2, 7, 3,
-        //     2, 6, 7
+        //     3, 1, 0,
+        //     3, 2, 1,
+        //     4, 3, 0,
+        //     4, 7, 3,
+        //     5, 7, 4,
+        //     5, 6, 7,
+        //     1, 4, 0,
+        //     5, 4, 1,
+        //     2, 5, 1,
+        //     2, 6, 5,
+        //     3, 7, 2,
+        //     7, 6, 2
         // ];
 
         this.positionCount = positions.length;
